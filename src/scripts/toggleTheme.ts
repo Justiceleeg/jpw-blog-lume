e4397c19e751fa080f08f4e742432834cb56f877
Justiceleeg <justicepwhite@gmail.com>
2024-08-27 11:44:26 -0500

    theme icon now toggles correctly

--- src/scripts/toggleTheme.ts
+++ src/scripts/toggleTheme.ts
@@ -1,26 +1,40 @@
+const themeIconToggle = document.getElementById("theme-icon-checkbox");
+// theme icon toggle is a checkbox (checked attribute == true => sun icon)
+const themeIconToggleLabel = document.getElementById(
+  "theme-icon-checkbox-label"
+);
 const lightThemeToggle = document.getElementById("light-theme-toggle");
 const darkThemeToggle = document.getElementById("dark-theme-toggle");
 const systemThemeToggle = document.getElementById("system-theme-toggle");
 
-const setDarkTheme = () =>
+const setDarkTheme = (isOnMount?: boolean) => {
   document.documentElement.setAttribute("data-theme", "dracula");
-const setLightTheme = () =>
+  if (themeIconToggle instanceof HTMLInputElement)
+    themeIconToggle.checked = false;
+  // necessary bc of loading the default state (moon) icon flash
+  if (isOnMount) themeIconToggleLabel?.classList.remove("hidden");
+};
+const setLightTheme = (isOnMount?: boolean) => {
   document.documentElement.setAttribute("data-theme", "light");
-const setThemeFromSystem = () => {
+  if (themeIconToggle instanceof HTMLInputElement)
+    themeIconToggle.checked = true;
+  if (isOnMount) themeIconToggleLabel?.classList.remove("hidden");
+};
+const setThemeFromSystem = (isOnMount?: boolean) => {
   if (globalThis.matchMedia("(prefers-color-scheme: dark)").matches) {
-    setDarkTheme();
+    setDarkTheme(isOnMount);
   } else {
-    setLightTheme();
+    setLightTheme(isOnMount);
   }
 };
 
 // Default button settings
 if (!("theme" in localStorage)) {
-  setThemeFromSystem();
+  setThemeFromSystem(true);
 } else if (localStorage.theme === "dark") {
-  setDarkTheme();
+  setDarkTheme(true);
 } else {
-  setLightTheme();
+  setLightTheme(true);
 }
 
 lightThemeToggle?.addEventListener("change", () => {
